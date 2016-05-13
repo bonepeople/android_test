1943acf135088cb24dbd07f51848b5aff39f4823
bonepeople <bonepeople@sina.com>
2016-05-13 10:05:54 +0800

    代码更新
    初步完成风格选择的页面布局

--- src/com/shownest/android/widget/LinearLayout_style.java
+++ src/com/shownest/android/widget/LinearLayout_style.java
@@ -5,6 +5,7 @@ import com.shownest.android.R;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -57,10 +58,12 @@ public class LinearLayout_style extends LinearLayout
 			_textview_item = new TextView(context);
 			_textview_item.setText(string);
 			_textview_item.setTextSize(18);
-			_textview_item.setPadding(10, 10, 10, 10);
+			_textview_item.setGravity(Gravity.CENTER);
 			_textview_item.setBackgroundResource(R.drawable.background_button_gray);
 			_params = new GridLayout.LayoutParams();
 			_params.setMargins(10, 10, 10, 10);
+			_params.width = 120;
+			_params.height = 50;
 			_gridlayout.addView(_textview_item, _params);
 		}
 
