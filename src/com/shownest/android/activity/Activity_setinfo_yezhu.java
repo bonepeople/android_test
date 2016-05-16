b9a4346207e6c2fb5bfe2b6f49436969bf65e943
bonepeople <bonepeople@sina.com>
2016-05-16 18:19:36 +0800

    优化代码
    修改activity的布局文件，将有变动的部分提取出来交给activity处理，剩下的内容可以进行复用

--- src/com/shownest/android/activity/Activity_setinfo_yezhu.java
+++ src/com/shownest/android/activity/Activity_setinfo_yezhu.java
@@ -5,7 +5,10 @@ import org.json.JSONObject;
 
 import com.shownest.android.R;
 import com.shownest.android.basic.DEBUG_Activity;
+import com.shownest.android.fragment.Fragment_setinfo_yezhu;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.RelativeLayout;
@@ -40,9 +43,14 @@ public class Activity_setinfo_yezhu extends DEBUG_Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_setinfo_yezhu);
+		setContentView(R.layout.activity_setinfo);
 		_instance = this;
 		_relativelayout_wait = (RelativeLayout) findViewById(R.id.relativelayout_wait);
+
+		FragmentManager fm = getFragmentManager();
+		FragmentTransaction tx = fm.beginTransaction();
+		tx.add(R.id.framelayout_content, new Fragment_setinfo_yezhu(), null);
+		tx.commit();
 	}
 
 	private static void handle_string(int _message, String _str)
