07dc0b9a6c122e0b7e0cfe1127a0916e94dc1399
bonepeople <bonepeople@sina.com>
2016-05-17 17:40:19 +0800

    功能更新
    添加自定义页签小控件，后期各个用户的身份认证会使用到。
    为配有页签的activity单独创建了一个布局，默认显示页签。

---
+++ src/com/shownest/android/widget/Linearlayout_subtitle.java
@@ -0,0 +1,87 @@
+package com.shownest.android.widget;
+
+import com.shownest.android.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Linearlayout_subtitle extends LinearLayout implements View.OnClickListener
+{
+	private onSelectListener _listener;
+	private int _selected = 1;
+	private TextView[] _text = new TextView[3];
+	private View[] _line = new View[3];
+
+	public interface onSelectListener
+	{
+		void onSelect(int _page);
+	}
+
+	public Linearlayout_subtitle(Context context)
+	{
+		super(context);
+	}
+
+	public void setOnSelectListener(onSelectListener _select_listener)
+	{
+		_listener = _select_listener;
+	}
+
+	public Linearlayout_subtitle(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		System.out.println("subtitle con");
+		View.inflate(context, R.layout.widget_subtitle, this);
+		System.out.println("subtitle inflate");
+		_text[0] = (TextView) findViewById(R.id.textview_subtitle_1);
+		_text[1] = (TextView) findViewById(R.id.textview_subtitle_2);
+		_text[2] = (TextView) findViewById(R.id.textview_subtitle_3);
+		_line[0] = findViewById(R.id.view_subtitle_1);
+		_line[1] = findViewById(R.id.view_subtitle_2);
+		_line[2] = findViewById(R.id.view_subtitle_3);
+		// 以下方式可以使用，在大量相似控件需要设置的时候可以放在循环里
+		// _line[0] = findViewById(getResources().getIdentifier("view_subtitle_"+"1", "id", context.getPackageName()));
+		// _line[1] = findViewById(getResources().getIdentifier("view_subtitle_"+"2", "id", context.getPackageName()));
+		// _line[2] = findViewById(getResources().getIdentifier("view_subtitle_"+"3", "id", context.getPackageName()));
+
+		int _temp_i = 0;
+		for (; _temp_i < _text.length; _temp_i++)
+			_text[_temp_i].setOnClickListener(this);
+	}
+
+	public Linearlayout_subtitle(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		int _select = 1;
+		switch (v.getId())
+		{
+		case R.id.textview_subtitle_1:
+			_select = 1;
+			break;
+		case R.id.textview_subtitle_2:
+			_select = 2;
+			break;
+		case R.id.textview_subtitle_3:
+			_select = 3;
+			break;
+		}
+		if (_select != _selected)
+		{
+			_text[_selected - 1].setTextColor(getResources().getColor(R.color.text_black));
+			_line[_selected - 1].setVisibility(View.INVISIBLE);
+			_text[_select - 1].setTextColor(getResources().getColor(R.color.main_color));
+			_line[_select - 1].setVisibility(View.VISIBLE);
+			_selected = _select;
+			if (_listener != null)
+				_listener.onSelect(_select);
+		}
+	}
+}
