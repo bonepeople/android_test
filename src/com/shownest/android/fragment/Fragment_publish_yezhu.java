7dffdcab1acca01905a48454fa3f905c5af28381
bonepeople <bonepeople@sina.com>
2016-05-18 11:48:49 +0800

    完善功能
    修改informationbar的布局，目前第2,3，5，6种类型的布局可以正常使用

--- src/com/shownest/android/fragment/Fragment_publish_yezhu.java
+++ src/com/shownest/android/fragment/Fragment_publish_yezhu.java
@@ -27,7 +27,7 @@ public class Fragment_publish_yezhu extends DEBUG_Fragment
 
 		RelativeLayout_edit_informationbar _item1 = new RelativeLayout_edit_informationbar(getActivity(), _linearlayout_body, 1, new String[] { "first", "left", "right" }, true);
 		RelativeLayout_edit_informationbar _item2 = new RelativeLayout_edit_informationbar(getActivity(), _linearlayout_body, 1, new String[] { "first", "left", "right" }, true);
-		RelativeLayout_edit_informationbar _item3 = new RelativeLayout_edit_informationbar(getActivity(), _linearlayout_body, 2, new String[] { "style2", "value" }, true);
+		RelativeLayout_edit_informationbar _item3 = new RelativeLayout_edit_informationbar(getActivity(), _linearlayout_body, 2, new String[] { "style2", "北京市 丰台区" }, true);
 		RelativeLayout_edit_informationbar _item4 = new RelativeLayout_edit_informationbar(getActivity(), _linearlayout_body, 3, new String[] { "style3", "number", "unit" }, true);
 		RelativeLayout_edit_informationbar _item5 = new RelativeLayout_edit_informationbar(getActivity(), _linearlayout_body, 4, new String[] { "style4", "1", "2", "3", "4", "5" }, true);
 
