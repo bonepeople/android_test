c404eb460781a61287ae5deb566b055849632f8f
bonepeople <bonepeople@sina.com>
2016-05-18 14:08:50 +0800

    功能完善
    添加上传身份证照片的控件

---
+++ src/com/shownest/android/widget/LinearLayout_idcard.java
@@ -0,0 +1,66 @@
+package com.shownest.android.widget;
+
+import com.shownest.android.model.OnSelectListener;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class LinearLayout_idcard extends LinearLayout implements View.OnClickListener, OnSelectListener
+{
+	private static boolean DEBUG = true;
+	private TextView _textview_name;
+
+	public LinearLayout_idcard(Context context)
+	{
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinearLayout_idcard(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinearLayout_idcard(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 初始化一个身份证上传控件
+	 * 
+	 * @param _name
+	 *            控件标题
+	 */
+	public LinearLayout_idcard(Context context, String _name)
+	{
+		super(context);
+		if (DEBUG)
+			System.out.println("LinearLayout_idcard super");
+
+		this.setOrientation(LinearLayout.VERTICAL);
+
+		_textview_name = new TextView(context);
+		_textview_name.setText(_name);
+		_textview_name.setTextSize(18);
+		_textview_name.setPadding(10, 0, 0, 0);
+		this.addView(_textview_name);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+
+	}
+
+	@Override
+	public void onSelect(int _index)
+	{
+		_textview_name.setText("selected : " + _index);
+	}
+}
