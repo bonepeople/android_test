a6ce7625bef34bb6dd63b64686dd5a9364a28de0
bonepeople <bonepeople@sina.com>
2016-06-04 16:41:27 +0800

    添加控件
    初步构建了LinearLayout_picture控件，主要用于显示和添加图片

--- src/com/shownest/android/fragment/Fragment_test.java
+++ src/com/shownest/android/fragment/Fragment_test.java
@@ -2,6 +2,7 @@ package com.shownest.android.fragment;
 
 import com.shownest.android.R;
 import com.shownest.android.basic.DEBUG_Fragment;
+import com.shownest.android.widget.LinearLayout_picture;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -19,7 +20,9 @@ public class Fragment_test extends DEBUG_Fragment
 		View _view = inflater.inflate(R.layout.fragment_test, container, false);
 
 		LinearLayout _body = (LinearLayout) _view.findViewById(R.id.linearlayout_content);
-
+		LinearLayout_picture _picture = new LinearLayout_picture(getActivity(), "pictures");
+		
+		_body.addView(_picture);
 		return _view;
 	}
 }
