2429ddf9274d08dcb567bb6799b81af6ad49390e
bonepeople <bonepeople@sina.com>
2016-06-04 18:45:53 +0800

    添加控件
    初步完成LinearLayout_picture控件的选择图片，显示图片功能
    补充上次提交未上传的LinearLayout_picture.java文件

--- src/com/shownest/android/fragment/Fragment_test.java
+++ src/com/shownest/android/fragment/Fragment_test.java
@@ -1,17 +1,32 @@
 package com.shownest.android.fragment;
 
+import java.io.File;
+
 import com.shownest.android.R;
 import com.shownest.android.basic.DEBUG_Fragment;
+import com.shownest.android.model.OnChangeListener;
 import com.shownest.android.widget.LinearLayout_picture;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class Fragment_test extends DEBUG_Fragment
+public class Fragment_test extends DEBUG_Fragment implements OnChangeListener
 {
+	private Uri _image_uri;
+	private LinearLayout_picture _picture;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -20,9 +35,118 @@ public class Fragment_test extends DEBUG_Fragment
 		View _view = inflater.inflate(R.layout.fragment_test, container, false);
 
 		LinearLayout _body = (LinearLayout) _view.findViewById(R.id.linearlayout_content);
-		LinearLayout_picture _picture = new LinearLayout_picture(getActivity(), "pictures");
-		
+		_picture = new LinearLayout_picture(getActivity(), "pictures");
+		_picture.setOnChangeListener(this);
+
 		_body.addView(_picture);
 		return _view;
 	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		System.out.println("requestCode=" + requestCode + "resultCode=" + resultCode);
+		if (resultCode == -1)
+		{
+			if (requestCode == 2)
+			{
+				_image_uri = data.getData();
+			}
+			else if(requestCode == 200)
+			{
+				Bitmap bmap = data.getParcelableExtra("data");  
+                
+                
+				_picture.add_image(bmap);
+				return;
+			}
+//			Intent intent = new Intent();  
+//            
+//            intent.setAction("com.android.camera.action.CROP");  
+//            intent.setDataAndType(_image_uri, "image/*");// mUri是已经选择的图片Uri  
+//            intent.putExtra("crop", "true");  
+//            intent.putExtra("aspectX", 1);// 裁剪框比例  
+//            intent.putExtra("aspectY", 1);  
+//            intent.putExtra("outputX", 150);// 输出图片大小  
+//            intent.putExtra("outputY", 150);  
+//            intent.putExtra("return-data", true);  
+//              
+//            startActivityForResult(intent, 200); 
+			_picture.add_image(_image_uri);
+		}
+	}
+
+	private void show_dialog()
+	{
+		final String[] _temp_str = new String[] { "拍摄照片", "选取照片", "取消" };
+
+		AlertDialog.Builder _builder = new Builder(getActivity());
+		_builder.setItems(_temp_str, new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				switch (which)
+				{
+				case 0:
+					try
+					{
+						Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+						intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+						intent.putExtra(MediaStore.EXTRA_OUTPUT, _image_uri);
+						startActivityForResult(intent, 1);
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+						Toast.makeText(getActivity(), "您的手机不具备拍照功能", Toast.LENGTH_SHORT).show();
+					}
+					break;
+				case 1:
+					try
+					{
+						Intent intent = new Intent();
+						intent.setType("image/*");
+						intent.setAction(Intent.ACTION_GET_CONTENT);
+						startActivityForResult(intent, 2);// data.getExtras()
+					}
+					catch (ActivityNotFoundException e)
+					{
+						Toast.makeText(getActivity(), "您的手机不具备选择图片的功能", Toast.LENGTH_SHORT).show();
+					}
+					break;
+				case 2:
+					break;
+				}
+			}
+		});
+		_builder.show();
+	}
+
+	@Override
+	public void onChange(String tag, String[] args)
+	{
+		if (args != null)
+		{
+			// see
+			Toast.makeText(getActivity(), tag + "-" + args[0], Toast.LENGTH_SHORT).show();
+		}
+		else
+		{
+			// add
+			Toast.makeText(getActivity(), tag, Toast.LENGTH_SHORT).show();
+			File _file_dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/shownest_cache");
+			File _file;
+			if (_file_dir.mkdirs() || _file_dir.isDirectory())
+			{
+				_file = new File(_file_dir, "left.tmp");
+				_image_uri = Uri.fromFile(_file);
+				show_dialog();
+			}
+			else
+				Toast.makeText(getActivity(), "获取缓存失败", Toast.LENGTH_SHORT).show();
+		}
+
+	}
+
 }
---
+++ src/com/shownest/android/widget/LinearLayout_picture.java
@@ -0,0 +1,197 @@
+package com.shownest.android.widget;
+
+import java.io.File;
+
+import com.shownest.android.R;
+import com.shownest.android.model.OnChangeListener;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LinearLayout_picture extends LinearLayout implements View.OnClickListener, View.OnLongClickListener
+{
+	private boolean _addable = true;
+	private GridLayout _body;
+	private int _image_count = 0;
+	private OnChangeListener _listener;
+	private int _picture_width = 150;
+	private int _picture_height = 150;
+	private int _picture_margins = 10;
+
+	public LinearLayout_picture(Context context)
+	{
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinearLayout_picture(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinearLayout_picture(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinearLayout_picture(Context context, String _name)
+	{
+		super(context);
+		this.setOrientation(LinearLayout.VERTICAL);
+
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		Point _outSize = new Point(0, 0);
+		wm.getDefaultDisplay().getSize(_outSize);
+		int screen_width = _outSize.x;
+		int screen_height = _outSize.y;
+
+		StringBuilder _builder = new StringBuilder();
+		_builder.append(_name + "\n");
+		_builder.append("screen_width=" + screen_width + " screen_height=" + screen_height);
+
+		TextView _textview_name = new TextView(context);
+		_textview_name.setText(_builder.toString());
+		_textview_name.setPadding(10, 0, 0, 0);
+		this.addView(_textview_name);
+
+		_body = new GridLayout(getContext());
+		int _ColumnCount = screen_width / (_picture_width + _picture_margins * 2);
+		_body.setColumnCount(_ColumnCount);
+		LinearLayout.LayoutParams _param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		_param.width = (_picture_width + _picture_margins * 2) * _ColumnCount;
+		_param.gravity = Gravity.CENTER_HORIZONTAL;
+		_body.setLayoutParams(_param);
+
+		if (_addable)
+		{
+			ImageView _image_add = new ImageView(getContext());
+			_image_add.setId(_image_count++);
+			_image_add.setBackgroundColor(getContext().getResources().getColor(R.color.text_blue));
+			_image_add.setOnClickListener(this);
+			GridLayout.LayoutParams _param_add = new GridLayout.LayoutParams();
+			_param_add.height = _picture_height;
+			_param_add.width = _picture_width;
+			_param_add.setMargins(_picture_margins, _picture_margins, _picture_margins, _picture_margins);
+			_image_add.setLayoutParams(_param_add);
+			_body.addView(_image_add);
+		}
+
+		this.addView(_body);
+
+	}
+
+	public void add_image(Uri _uri)
+	{
+		if (_uri != null)
+		{
+			File _imagefile = new File(_uri.getPath());
+			Bitmap image = getSmallBitmap(_imagefile.getAbsolutePath(), _picture_width, _picture_height);
+
+			if (image != null)
+			{
+				ImageView _imageview = new ImageView(getContext());
+				_imageview.setId(_image_count++);
+				_imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				_imageview.setImageBitmap(image);
+				_imageview.setOnClickListener(this);
+				_imageview.setOnLongClickListener(this);
+				GridLayout.LayoutParams _param = new GridLayout.LayoutParams();
+				_param.height = _picture_height;
+				_param.width = _picture_width;
+				_param.setMargins(_picture_margins, _picture_margins, _picture_margins, _picture_margins);
+				_imageview.setLayoutParams(_param);
+				_body.addView(_imageview, _body.getChildCount() - 1);
+			}
+		}
+	}
+
+	public void add_image(Bitmap _image)
+	{
+		if (_image != null)
+		{
+			ImageView _imageview = new ImageView(getContext());
+			_imageview.setId(_image_count++);
+			_imageview.setImageBitmap(_image);
+			_imageview.setOnClickListener(this);
+			_imageview.setOnLongClickListener(this);
+			GridLayout.LayoutParams _param = new GridLayout.LayoutParams();
+			_param.height = _picture_height;
+			_param.width = _picture_width;
+			_param.setMargins(_picture_margins, _picture_margins, _picture_margins, _picture_margins);
+			_imageview.setLayoutParams(_param);
+			_body.addView(_imageview, _body.getChildCount() - 1);
+		}
+	}
+
+	private static Bitmap getSmallBitmap(String filePath, int _width, int _height)
+	{
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(filePath, options);
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, _width, _height);
+
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+
+		return BitmapFactory.decodeFile(filePath, options);
+	}
+
+	private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
+	{
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth)
+		{
+			final int heightRatio = Math.round((float) height / (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+		return inSampleSize;
+	}
+
+	public void setOnChangeListener(OnChangeListener _listener)
+	{
+		this._listener = _listener;
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		int _id = v.getId();
+		if (_addable && _id == 0)
+		{
+			if (_listener != null)
+				_listener.onChange("add", null);
+		}
+		else
+		{
+			if (_listener != null)
+				_listener.onChange("see", new String[] { String.valueOf(_id) });
+		}
+	}
+
+	@Override
+	public boolean onLongClick(View v)
+	{
+		int _id = v.getId();
+		Toast.makeText(getContext(), "image_" + _id + " has been Longclicked", Toast.LENGTH_SHORT).show();
+		return true;
+	}
+}
