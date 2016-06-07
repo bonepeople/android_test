34e434bdc0628bd6a14c96d06aca6c3827e5f467
bonepeople <bonepeople@sina.com>
2016-06-07 14:53:41 +0800

    界面更新
    仅仅是方便测试

--- src/com/shownest/android/fragment/Fragment_login.java
+++ src/com/shownest/android/fragment/Fragment_login.java
@@ -22,7 +22,7 @@ public class Fragment_login extends DEBUG_Fragment
 {
 	private EditText _edittext_username, _edittext_password;
 	private TextView _textview_forget;
-	private Button _button_login;
+	private Button _button_login, _yezhu, _shigongdui, _shejishi, _jianli;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
@@ -32,7 +32,48 @@ public class Fragment_login extends DEBUG_Fragment
 		_edittext_username = (EditText) _view.findViewById(R.id.edittext_username);
 		_edittext_password = (EditText) _view.findViewById(R.id.edittext_password);
 		_textview_forget = (TextView) _view.findViewById(R.id.textview_forget_pwd);
-
+		// =================测试使用
+		_yezhu = (Button) _view.findViewById(R.id.button_yezhu);
+		_shigongdui = (Button) _view.findViewById(R.id.button_shigongdui);
+		_shejishi = (Button) _view.findViewById(R.id.button_shejishi);
+		_jianli = (Button) _view.findViewById(R.id.button_jianli);
+		_yezhu.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				_edittext_username.setText("15901515556");
+				_edittext_password.setText("jiangsi8878");
+			}
+		});
+		_shigongdui.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				_edittext_username.setText("13788816776");
+				_edittext_password.setText("111111");
+			}
+		});
+		_shejishi.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				_edittext_username.setText("13760313770");
+				_edittext_password.setText("111111");
+			}
+		});
+		_jianli.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				_edittext_username.setText("15072058793");
+				_edittext_password.setText("111111");
+			}
+		});
+		// =================测试使用
 		_button_login.setOnClickListener(new OnClickListener()
 		{
 			@Override
