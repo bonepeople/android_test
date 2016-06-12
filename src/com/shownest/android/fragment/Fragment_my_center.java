11a21621e4b3bb0e4ee5f2247d20e7a7e1d049e6
bonepeople <bonepeople@sina.com>
2016-06-12 15:45:01 +0800

    完善界面
    编辑个人中心的布局，将需要显示的图标都预留出来
    图标图片更新

--- src/com/shownest/android/fragment/Fragment_my_center.java
+++ src/com/shownest/android/fragment/Fragment_my_center.java
@@ -14,13 +14,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Fragment_my_center extends DEBUG_Fragment implements View.OnClickListener
 {
 	private TextView _textview_money, _textview_name, _number_bowen, _number_guanzhu, _number_xiuyou, _textview_hint;
-	private RelativeLayout _item_info, _item_money;
+	private TextView _zhaobiao, _zhaobiao_comment, _yuebiao_comment;
+	private LinearLayout _line1, _line2, _line3;
+	private RelativeLayout _item_bid, _item_offer, _item_zhuangxiu, _item_fangwu, _item_baojia, _item_gongdi, _item_info, _item_money;
 	private SmartImageView _imageview_header;
 
 	@Override
@@ -35,9 +39,30 @@ public class Fragment_my_center extends DEBUG_Fragment implements View.OnClickLi
 		_number_guanzhu = (TextView) _view.findViewById(R.id.textview_number_guanzhu);
 		_number_xiuyou = (TextView) _view.findViewById(R.id.textview_number_xiuyou);
 		_textview_hint = (TextView) _view.findViewById(R.id.textview_hint);
+		_line1 = (LinearLayout) _view.findViewById(R.id.linearlayout_center_line1);
+		_line2 = (LinearLayout) _view.findViewById(R.id.linearlayout_center_line2);
+		_line3 = (LinearLayout) _view.findViewById(R.id.linearlayout_center_line3);
+		_item_bid = (RelativeLayout) _view.findViewById(R.id.item_bid);
+		_item_offer = (RelativeLayout) _view.findViewById(R.id.item_offer);
+		_item_zhuangxiu = (RelativeLayout) _view.findViewById(R.id.item_zhuangxiu);
+		_item_fangwu = (RelativeLayout) _view.findViewById(R.id.item_fangwu);
+		_item_baojia = (RelativeLayout) _view.findViewById(R.id.item_baojia);
+		_item_gongdi = (RelativeLayout) _view.findViewById(R.id.item_gongdi);
 		_item_info = (RelativeLayout) _view.findViewById(R.id.item_info);
 		_item_money = (RelativeLayout) _view.findViewById(R.id.item_money);
+		_zhaobiao = (TextView) _view.findViewById(R.id.textview_name_bid);
+		_zhaobiao_comment = (TextView) _view.findViewById(R.id.textview_comment_bid);
+		_yuebiao_comment = (TextView) _view.findViewById(R.id.textview_comment_offer);
 		_imageview_header = (SmartImageView) _view.findViewById(R.id.imageview_header);
+
+		_item_bid.setOnClickListener(this);
+		_item_offer.setOnClickListener(this);
+		_item_zhuangxiu.setOnClickListener(this);
+		_item_fangwu.setOnClickListener(this);
+		_item_baojia.setOnClickListener(this);
+		_item_gongdi.setOnClickListener(this);
+		_item_info.setOnClickListener(this);
+		_item_money.setOnClickListener(this);
 
 		return _view;
 	}
@@ -56,22 +81,24 @@ public class Fragment_my_center extends DEBUG_Fragment implements View.OnClickLi
 		switch (_info.get_userType())
 		{
 		case 11:
+			_line1.setVisibility(LinearLayout.VISIBLE);
+			_line2.setVisibility(LinearLayout.VISIBLE);
 			break;
 		case 12:
-			break;
 		case 13:
-			break;
 		case 14:
-			break;
 		case 15:
+			_line1.setVisibility(LinearLayout.VISIBLE);
+			_line3.setVisibility(LinearLayout.VISIBLE);
+			_zhaobiao.setText("我的投标");
+			_zhaobiao_comment.setText("查看您的投标信息，建立托管协议");
+			_yuebiao_comment.setText("查看业主对您发起的预约，建立托管协议");
 			break;
 		case 100:
 			_textview_hint.setVisibility(TextView.VISIBLE);
 			_textview_hint.setOnClickListener(this);
 			break;
 		}
-		_item_info.setOnClickListener(this);
-		_item_money.setOnClickListener(this);
 	}
 
 	@Override
@@ -83,14 +110,38 @@ public class Fragment_my_center extends DEBUG_Fragment implements View.OnClickLi
 			Intent _select_role = new Intent(getActivity(), Activity_select_role.class);
 			getActivity().startActivity(_select_role);
 			break;
+
+		case R.id.item_bid:
+			Toast.makeText(getActivity(), "招标", Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.item_offer:
+			Toast.makeText(getActivity(), "约标", Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.item_zhuangxiu:
+			Toast.makeText(getActivity(), "装修", Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.item_fangwu:
+			Toast.makeText(getActivity(), "房屋", Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.item_baojia:
+			Toast.makeText(getActivity(), "报价", Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.item_gongdi:
+			Toast.makeText(getActivity(), "工地", Toast.LENGTH_SHORT).show();
+			break;
 		case R.id.item_info:
 			Intent _my_info = new Intent(getActivity(), Activity_basicinfo.class);
 			getActivity().startActivity(_my_info);
 			break;
 
 		case R.id.item_money:
+			Toast.makeText(getActivity(), "钱包", Toast.LENGTH_SHORT).show();
 			break;
 		}
-
 	}
 }
