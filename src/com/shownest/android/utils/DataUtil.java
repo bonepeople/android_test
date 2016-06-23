9411cd524ebca28f154b3e9cf550872422f550db
bonepeople <bonepeople@sina.com>
2016-06-23 14:48:26 +0800

    添加注释

--- src/com/shownest/android/utils/DataUtil.java
+++ src/com/shownest/android/utils/DataUtil.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
+/**
+ * 操作SharedPreferences的工具类
+ * 
+ * @author bonepeople
+ */
 public class DataUtil
 {
-
+	/**
+	 * 从config.xml文件中读取ukey字段，若为空则返回<b>null</b>
+	 */
 	public static String get_ukey(Context _context)
 	{
 		SharedPreferences sharedPreferences = _context.getSharedPreferences("config", Context.MODE_PRIVATE);
@@ -16,14 +23,19 @@ public class DataUtil
 		return name.equals("") ? null : name;
 	}
 
+	/**
+	 * 将ukey写入config.xml文件中
+	 * 
+	 * @param _ukey
+	 */
 	public static void set_ukey(Context _context, String _ukey)
 	{
 		SharedPreferences sharedPreferences = _context.getSharedPreferences("config", Context.MODE_PRIVATE);
 
-		Editor editor = sharedPreferences.edit();// 获取编辑器
+		Editor editor = sharedPreferences.edit();
 
 		editor.putString("ukey", _ukey);
 
-		editor.commit();// 提交修改
+		editor.commit();
 	}
 }
