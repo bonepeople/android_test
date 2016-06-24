a10930805d0ed232be9961f3f1ecb8f5a086fa1d
bonepeople <bonepeople@sina.com>
2016-06-24 16:40:13 +0800

    添加接口
    添加查询银行卡信息的接口

--- src/com/shownest/android/utils/HttpUtil.java
+++ src/com/shownest/android/utils/HttpUtil.java
@@ -32,6 +32,19 @@ public class HttpUtil
 	// http://192.168.1.112:10000/shownest/html/test1.html
 	// http://192.168.1.112:10000/shownest/websubmitreg
 	/**
+	 * 查询银行卡号对应的银行卡类别
+	 */
+	public static void get_card_type(Handler _handler, ContentValues _value, int _successful, int _failed)
+	{
+		String _address = BASEADDRESS + "webBankCardCheck";
+		String _message = "";
+
+		_message = values(_value);
+
+		new Thread_http(_handler, _address, _message, _successful, _failed, "POST").start();
+	}
+
+	/**
 	 * 获取订单详情，会根据当前登录用户的类别进行判断
 	 */
 	public static void get_order_detail(Handler _handler, ContentValues _value, int _successful, int _failed)
